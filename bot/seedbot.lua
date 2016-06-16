7cb535f556eb4d33e04557ecf084735aff710610
pouyamohamadi <parsasafa23@gmail.com>
2016-06-17 01:19:48 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {230283641},--Sudo users
+    sudo_users = {189240785},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
